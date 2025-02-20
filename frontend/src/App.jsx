5a7ce618f71bfb08e9acd9ab95887ff2781b0266
Bwrai <manveer.bwrai@gmail.com>
2025-02-20 15:58:00 +0530

    Added Loading visual for products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,8 @@
-// import Home from "./components/Home/Home";
-// import Footer from "./components/layout/Footer/Footer";
-// import Header from "./components/layout/Header/Header";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Header />
-//       <Home />
-//       <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
+import Loader from "./components/layout/Loader/Loader";
 
 function App() {
   return (
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,37 +4,42 @@ import "./Home.css";
 import Metadata from "../layout/Metadata.jsx"
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, getProducts } from "../../features/productSlice.js";
+import Loader from "../layout/Loader/Loader.jsx";
 
 
 function Home() {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.product)
+  const { products, loading, productsCount } = useSelector((state) => state.product)
 
   useEffect(() => {
     dispatch(getProducts())
 
     return () => {
-      if(products.length === 0) dispatch(clearErrors())
+      if (products.length === 0) dispatch(clearErrors())
     }
   }, [dispatch])
   return (
     <>
-      <Metadata title="BwraiMart" />
-      <div className="banner">
-        <p>Welcome to BwraiMart</p>
-        <h1>Shopping made easy with BwraiMart</h1>
-      </div>
-      <div>
-        <h2 className="productHeading">Featured Products</h2>
-      </div>
-      <div className="container">
+      {loading ? <Loader /> :
+        <>
+          <Metadata title="BwraiMart" />
+          <div className="banner">
+            <p>Welcome to BwraiMart</p>
+            <h1>Shopping made easy with BwraiMart</h1>
+          </div>
+          <div>
+            <h2 className="productHeading">Featured Products</h2>
+          </div>
+          <div className="container">
 
-        {
-          products && products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))
-        }
-      </div>
+            {
+              products && products.map((product) => (
+                <Product key={product._id} product={product} />
+              ))
+            }
+          </div>
+        </>
+      }
     </>
   );
 }
